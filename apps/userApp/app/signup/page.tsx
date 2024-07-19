270e677158afc2d9a9f54e04ccc314f6c34e6606
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-19 23:31:53 +0500

    Created Signup Page

--- apps/userApp/app/signup/page.tsx
+++ apps/userApp/app/signup/page.tsx
@@ -1,7 +1,15 @@
 export function Signup(){
     return <div>
-        <form action="
-        "></form>
-        Signup Page
+        <form>
+            <label>Username</label>
+            <input type="text"></input>
+            <label>email</label>
+            <input type="email"></input>
+            <label>password</label>
+            <input type="password"></input>
+            <label>confirm password</label>
+            <input type="password"></input>
+            <button>Submit</button>
+        </form>
     </div>
 }
