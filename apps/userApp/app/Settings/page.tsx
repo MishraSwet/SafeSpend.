a5302cff806d4a9162287dcc189ec57f57ec447c
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-26 23:53:55 +0530

    Added Settings Form

--- apps/userApp/app/Settings/page.tsx
+++ apps/userApp/app/Settings/page.tsx
@@ -1,5 +1,11 @@
 export default function Settings() {
     return <div>
-        Settings
+        Settings Page
+    </div>
+}
+
+export function SettingsForm() {
+    return <div>
+        <label htmlFor=""></label>
     </div>
 }
