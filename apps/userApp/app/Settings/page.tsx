9f7b109c0b0f62b8bab3714f1c652f2a19340340
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-27 23:58:57 +0530

    Added Settings Form Interface

--- apps/userApp/app/Settings/page.tsx
+++ apps/userApp/app/Settings/page.tsx
@@ -1,12 +1,15 @@
+interface Settingform{
+    label: string,
+}
 export default function Settings() {
     return <div>
         Settings Page
     </div>
 }
 
-export function SettingsForm() {
+export function SettingsForm({label}:Settingform) {
     return <div>
-        <label htmlFor=""></label>
+        <label htmlFor="">{label}</label>
         <input type="text" />
     </div>
 }
