810799de8fa375c3738c6953f61b123a950332df
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-30 11:56:27 +0530

    Finished TransactionCard Component

--- apps/userApp/app/Transactions/page.tsx
+++ apps/userApp/app/Transactions/page.tsx
@@ -16,8 +16,9 @@ interface Transactionprops{
 }
 
 export default function Transaction() {
+    
     const [data, setData] = useState<Transaction[]>([
-        { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }
+        { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }, { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }, { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }, { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }, { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }, { id: "1", date: "1/1/24", desc: "desc", status: "status", time: "10:24", amount: "6000" }
     ]);
 
     return <div>
@@ -66,16 +67,21 @@ export function NavText({ children,href}: { children: React.ReactNode,href:strin
     
 }
 
-export function NavBtn() {
-    return <button className="h-8 w-8">
 
-    </button>
-}
-
-export function TransactionCard({ transaction } :Transactionprops) {
-    return <div className="flex items-center justify-center bg-red-200 h-16">
-        <div className="flex flex-col items-center justify-center w-1/2"></div>
-        <div className="flex flex-col items-center justify-center w-1/2"></div>
-    </div>
+export function TransactionCard({ transaction }: Transactionprops) {
+    return (
+        <div className="flex sm:flex-row justify-end bg-purple-200 w-full sm:w-11/12 md:w-3/4 lg:w-1/2 min-h-[6rem] my-5 mx-auto p-4 shadow-md rounded-md">
+            <div className="flex flex-col justify-center items-start w-full sm:w-1/2 mb-2 sm:mb-0">
+                <p className="text-md font-semibold">{transaction.desc}</p>
+                <p className="text-xs text-gray-600">{transaction.date} - {transaction.time}</p>
+            </div>
+            <div className="flex flex-col justify-center items-end w-full sm:w-1/2">
+                <p className="text-lg font-bold">{transaction.amount}</p>
+                <p className={`text-md font-semibold ${transaction.status === 'completed' ? 'text-green-600' : 'text-red-600'}`}>
+                    {transaction.status}
+                </p>
+            </div>
+        </div>
+    );
 }
 
