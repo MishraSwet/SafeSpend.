fbc8ffd1582a054cb0e7f580153defd3b29123d7
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-08-19 23:57:22 +0530

    Added Transaction Interaface for Type

--- apps/merchantApp/app/Transactions/Page.tsx
+++ apps/merchantApp/app/Transactions/Page.tsx
@@ -1,3 +1,16 @@
+interface Transaction {
+  id: string,
+  date: string,
+  desc: string,
+  status: string,
+  time: string,
+  amount: string
+}
+
+interface Transactionprops {
+  transaction: Transaction
+}
+
 export default function Transactions() {
   return (
     <div>
