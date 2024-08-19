c22accbf45f563fa3652ef48ff94ea1ccdfa6702
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-08-19 23:58:00 +0530

    Added Transaction Card

--- apps/merchantApp/app/Transactions/Page.tsx
+++ apps/merchantApp/app/Transactions/Page.tsx
@@ -19,3 +19,20 @@ export default function Transactions() {
   )
 }
 
+export function TransactionCard({ transaction }: Transactionprops) {
+  return (
+    <div className="flex sm:flex-row justify-end bg-purple-200 w-full sm:w-11/12 md:w-3/4 lg:w-1/2 min-h-[6rem] my-5 mx-auto p-4 shadow-md rounded-md">
+      <div className="flex flex-col justify-center items-start w-full sm:w-1/2 mb-2 sm:mb-0">
+        <p className="text-md font-semibold">{transaction.desc}</p>
+        <p className="text-xs text-gray-600">{transaction.date} - {transaction.time}</p>
+      </div>
+      <div className="flex flex-col justify-center items-end w-full sm:w-1/2">
+        <p className="text-lg font-bold">{transaction.amount}</p>
+        <p className={`text-md font-semibold ${transaction.status === 'completed' ? 'text-green-600' : 'text-red-600'}`}>
+          {transaction.status}
+        </p>
+      </div>
+    </div>
+  );
+}
+
